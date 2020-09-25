22b22d2cdaa327b87fb1b8c2f97081e47b9aabf6
Tatiane Fukuda <tatiane.fukuda@gmail.com>
2020-09-24 22:49:22 -0300

    Update create_tables.js
    Atualizando as preferências que são exibidas para o usuário

--- infra/create_tables.js
+++ infra/create_tables.js
@@ -4,7 +4,7 @@ const conn = mysql.createConnection({ host: process.env.DB_HOST, user: process.e
 (() => {
     conn.query("SET FOREIGN_KEY_CHECKS=0");
     conn.query("CREATE TABLE IF NOT EXISTS `preferences` (`id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(50) NOT NULL, `description` VARCHAR(100) NOT NULL, PRIMARY KEY (`id`), UNIQUE INDEX `name_UNIQUE` (`name` ASC))");
-    conn.query("REPLACE INTO `preferences` (`id`, `name`, `description`) VALUES (1, 'apenas_sexo_feminino','Motorias do sexo feminino')");
+    conn.query("REPLACE INTO `preferences` (`id`, `name`, `description`) VALUES (1, 'apenas_sexo_feminino','Motoristas do sexo feminino')");
     conn.query("REPLACE INTO `preferences` (`id`, `name`, `description`) VALUES (2, 'revisar_preferencias','Revisar opções a cada corrida')");
     conn.query("REPLACE INTO `preferences` (`id`, `name`, `description`) VALUES (3, 'compartilhar_contatos','Compartilhar automaticamente minha corrida com os contatos de segurança')");
     conn.query("REPLACE INTO `preferences` (`id`, `name`, `description`) VALUES (4, 'area_risco','Evitar áreas de risco')");
